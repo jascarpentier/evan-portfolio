0c5385e9ff5e528be6c823196957c8e82589d464
jasmine carpentier <jascarpentier@gmail.com>
2019-09-25 23:14:35 -0400

    init

---
+++ src/components/Main.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Main extends React.Component {
+  render() {
+    return (
+      <div className='mainStuff'>
+
+      </div>
+    )
+  }
+}
+
+export default Main;
